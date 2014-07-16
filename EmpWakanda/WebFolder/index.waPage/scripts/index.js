6fdbad02649faeb40928c7fde295648158872f44
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-07-16 15:59:01 +0900

    Initial commit

---
+++ EmpWakanda/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,21 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var Double_salaries = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	Double_salaries.click = function Double_salaries_click (event)// @startlock
+	{// @endlock
+		sources.employees.DoubleSalary({
+			onSuccess:function(event){
+				sources.employees.allEntities();
+			}});
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("Double_salaries", "click", Double_salaries.click, "WAF");
+// @endregion
+};// @endlock
